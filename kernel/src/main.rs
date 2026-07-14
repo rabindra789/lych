9d1796d25fd429ea3fd2bf8b570b371b4d3e2434
rabindra789 <rabindrameher116@gmail.com>
2026-07-14 23:30:03 +0530

    kernel: add Rust entry point

---
+++ kernel/src/main.rs
@@ -0,0 +1,19 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+/// First Rust func to executed by the kernel
+#[unsafe(no_mangle)]
+pub extern "C" fn kernel_main() -> ! {
+    loop {
+        core::hint::spin_loop();
+    }
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop{
+        core::hint::spin_loop();
+    }
+}
