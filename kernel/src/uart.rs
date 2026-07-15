a46bd87cce40001b0e625cf82de4052ba49b884f
rabindra789 <rabindrameher116@gmail.com>
2026-07-15 10:42:26 +0530

    drivers: add PL011 UART putc

---
+++ kernel/src/uart.rs
@@ -0,0 +1,13 @@
+const UART0_BASE: usize = 0x0900_0000;
+
+const UART_DR: *mut u32 = UART0_BASE as *mut u32;
+const UART_FR: *const u32 = (UART_BASE + 0x18) as *const u32;
+
+const UART_FR_TXFF: u32 = i << 5;
+
+pub fn putc(c: u8) {
+    unsafe {
+        while UART_FR.read_volatile() & UART_FR_TXFF != 0 {}
+        UART_DR.write_volatile(c as u32);
+    }
+}
